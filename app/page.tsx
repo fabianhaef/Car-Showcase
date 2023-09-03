a5fe52f4e58f0674b62bae8ba866c960bc5bcfa2
fabianhaef <fabian.haefi@gmail.com>
2023-09-03 22:21:49 +0200

    Create Car Card and Details components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import { Hero,SearchBar, CustomFilter } from '@/components';
+import { Hero,SearchBar, CustomFilter, CarCard } from '@/components';
+import { fetchCars } from '@/utils';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -15,6 +19,18 @@ export default function Home() {
               <CustomFilter title="year"/>
             </div>
           </div>
+          {!isDataEmpty ? (
+            <section>
+              <div className='home__cars-wrapper'>
+                {allCars?.map((car) => <CarCard car={car} /> )}
+              </div>
+            </section>
+          ) : (
+            <div className='home__error-container'>
+              <h3 className='text-black text-xl'>Oops, no results!</h3>
+              <p>{allCars?.message}</p>
+            </div>
+          )}
         </div>
       </div>
     </main>
