f6d18a369dc764e2cdd939c75f732cca18e9ec23
fabianhaef <fabian.haefi@gmail.com>
2023-09-03 20:52:14 +0200

    make SearchManufacturer Comp more usable

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -17,7 +17,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
     return (
         <div className='search-manufacturer'>
-            <Combobox>
+            <Combobox value={manufacturer} onChange={setManufacturer}>
                 <div className="relative w-full">
                     <Combobox.Button className="absolute top-[14px]">
                         <Image src="/car-logo.svg" width={20} height={20} className='ml-4' alt="Car-Logo" />
@@ -37,7 +37,23 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                         <Combobox.Options>
                             {filteredManufacturers.map((item) => (
                                 <Combobox.Option value={item} key={item} className={({ active }) => `relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-primary-blue'}`}>
-                                    {item}
+                                    {({ selected, active }) => (
+                                        <>
+                                            <span
+                                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                                    }`}
+                                            >
+                                                {item}
+                                            </span>
+                                            {selected ? (
+                                                <span
+                                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'
+                                                        }`}
+                                                >
+                                                </span>
+                                            ) : null}
+                                        </>
+                                    )}
                                 </Combobox.Option>
                             ))}
                         </Combobox.Options>
