726f08cf5a534148ad4ac9ab7255785e7c0fea90
fabianhaef <fabian.haefi@gmail.com>
2023-09-03 22:40:10 +0200

    Implement imagin.studio api for car images

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers = {
-		'X-RapidAPI-Key': 'fd1ebd30c8mshad457688370d617p13267fjsneffd13a385f2',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
+        'X-RapidAPI-Key': 'fd1ebd30c8mshad457688370d617p13267fjsneffd13a385f2',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
         headers: headers,
     });
 
@@ -17,13 +19,28 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
     const mileageFactor = 0.1; // Additional rate per mile driven
     const ageFactor = 0.05; // Additional rate per year of vehicle age
-  
+
     // Calculate additional rate based on mileage and age
     const mileageRate = city_mpg * mileageFactor;
     const ageRate = (new Date().getFullYear() - year) * ageFactor;
-  
+
     // Calculate total rental rate per day
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-  
+
     return rentalRatePerDay.toFixed(0);
-  };
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+  
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || 'hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+  
+    return `${url}`;
+  } 
